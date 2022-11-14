a9bdaab83ac0a550451473d068faef089832ce40
Shiao-Li <roberto.shiao@epn.edu.ec>
2022-11-14 11:09:49 -0500

    cambios agregados

--- src/Universidades.java
+++ src/Universidades.java
@@ -3,7 +3,7 @@ public class Universidades {
     int cant_estu;
     String ranking;
     int numAulass;
-    String hola;
+    String carreras;
     String hola2;
     String hola3;
     String hola4;
